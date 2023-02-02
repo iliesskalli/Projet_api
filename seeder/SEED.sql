ce65adf86a97e0213aca89ad99ac8d24b0e0c348
iliesskalli <iliesskalli2@gmail.com>
2023-02-02 14:48:06 +0100

    ok

---
+++ seeder/SEED.sql
@@ -0,0 +1,108 @@
+INSERT INTO categories (nom, description,date_creation)
+VALUES
+  ('Electronique', 'Produits électroniques tels que les téléphones, ordinateurs, etc.','2023-02-02'),
+  ('Vêtements', 'Vêtements pour hommes, femmes et enfants','2023-02-02'),
+  ('Livres', 'Livres pour tous les âges et genres','2023-02-02'),
+  ('Maison et Jardin', 'Articles pour la maison et le jardinage','2023-02-02'),
+  ('Beauté et Soins', 'Produits de beauté et soins personnels','2023-02-02'),
+  ('Alimentation', 'Aliments pour toutes les occasions','2023-02-02'),
+  ('Sports et Loisirs', 'Équipements pour le sport et les loisirs','2023-02-02'),
+  ('Jouets et Jeux', 'Jouets et jeux pour enfants et adultes','2023-02-02'),
+  ('Automobile', 'Pièces et accessoires pour voitures','2023-02-02'),
+  ('Musique', 'Instruments de musique et accessoires','2023-02-02'),
+  ('Voyage', 'Voyages, hôtels, billets avion et autres produits liés aux voyages','2023-02-02'),
+  ('Meubles', 'Meubles pour la maison et le bureau','2023-02-02'),
+  ('Bricolage', 'Outils et matériaux pour le bricolage','2023-02-02'),
+  ('Animaux', 'Produits pour animaux de compagnie','2023-02-02'),
+  ('Électroménager', 'Appareils électroménagers pour la maison','2023-02-02'),
+  ('Jeux vidéo', 'Jeux vidéo pour toutes les plateformes','2023-02-02'),
+  ('Art et Artisanat', 'Art et produits artisanaux','2023-02-02'),
+  ('Aliments pour animaux', 'Aliments pour animaux de compagnie','2023-02-02'),
+  ('Fleurs et Plantes', 'Fleurs et plantes pour intérieur et extérieur','2023-02-02'),
+  ('Informatique', 'Matériel et logiciels informatiques','2023-02-02'),
+  ('High-tech', 'Produits high-tech tels que les appareils photo, drones, etc.','2023-02-02'),
+  ('Automobile', 'Voitures, camions et autres véhicules','2023-02-02'),
+  ('Bijoux', 'Bijoux en or, argent, diamants, etc.','2023-02-02'),
+  ('Fournitures de bureau', 'Fournitures de bureau telles que des stylos, du papier, etc.','2023-02-02'),
+  ('Art de la table', 'Articles pour la cuisine et la salle à manger','2023-02-02'),
+  ('Luminaires', 'Luminaires pour la maison et le jardin','2023-02-02'),
+  ('Jardinage', 'Outils et matériaux pour le jardinage','2023-02-02'),
+  ('Puériculture', 'Produits pour les bébés et les enfants','2023-02-02'),
+  ('Santé et bien-être', 'Produits pour la santé et le bien-être','2023-02-02'),
+  ('Outils', 'Outils pour le bricolage et le jardinage','2023-02-02'),
+  ('Beauté et soins personnels', 'Produits de beauté et de soins personnels','2023-02-02'),
+  ('Sport et fitness', 'Equipements et vêtements pour le sport et le fitness','2023-02-02'),
+  ('Maison et décoration', 'Articles pour la maison et la décoration','2023-02-02'),
+  ('Bagages', 'Bagages pour les voyages','2023-02-02'),
+  ('Audio et vidéo', 'Produits audio et vidéo pour la maison','2023-02-02'),
+  ('Livres', 'Livres pour tous les genres et tous les âges','2023-02-02'),
+  ('Jouets et jeux', 'Jouets et jeux pour les enfants','2023-02-02'),
+  ('Chaussures', 'Chaussures pour hommes, femmes et enfants','2023-02-02'),
+  ('Vêtements', 'Vêtements pour hommes, femmes et enfants','2023-02-02'),
+  ('Accessoires de mode', 'Accessoires de mode tels que des bijoux, des lunettes, etc.','2023-02-02'),
+  ('Mobilier', 'Meubles pour la maison et le bureau','2023-02-02'),
+  ('Vélos et accessoires', 'Vélos et accessoires pour les cyclistes','2023-02-02'),
+  ('Voyages', 'Produits pour les voyages','2023-02-02'),
+  ('Jeux vidéo', 'Jeux vidéo pour les consoles et les ordinateurs','2023-02-02'),
+  ('Instruments de musique', 'Instruments de musique tels que les guitares, les pianos, etc.','2023-02-02'),
+  ('Cuisine et arts culinaires', 'Produits pour la cuisine et les arts culinaires','2023-02-02'),
+  ('Bricolage', 'Outils et matériaux pour le bricolage','2023-02-02'),
+  ('Animaux', 'Produits pour les animaux domestiques','2023-02-02'),
+  ('Fleurs et plantes', 'Fleurs et plantes pour la maison et le jardin','2023-02-02'),
+  ('Aliments et boissons', 'Aliments et boissons pour le petit déjeuner, le déjeuner et le dîner','2023-02-02');
+
+
+
+
+
+
+INSERT INTO produits (nom, description, prix,date_creation)
+VALUES
+  ('Ordinateur portable', 'Ordinateur portable de haute qualité avec des caractéristiques puissantes', 999,'2023-02-02'),
+  ('Smartphone', 'Smartphone avec un écran de haute résolution et des fonctionnalités avancées', 799),'2023-02-02',
+  ('Téléviseur', 'Téléviseur LED de haute qualité avec une image claire et brillante', 599,'2023-02-02'),
+  ('Tablette', 'Tablette légère et portable avec une longue durée de vie de la batterie', 399,'2023-02-02'),
+  ('Appareil photo numérique', 'Appareil photo numérique professionnel avec des fonctionnalités avancées', 749,'2023-02-02'),
+  ('Casque audio', 'Casque audio sans fil confortable avec un son de qualité supérieure', 199,'2023-02-02'),
+  ('Enceinte Bluetooth', 'Enceinte Bluetooth portable avec un son puissant et clair', 99,'2023-02-02'),
+  ('Lampe de bureau', 'Lampe de bureau moderne et élégante avec un design soigné', 49,'2023-02-02'),
+  ('Aspirateur', 'Aspirateur puissant et silencieux avec des filtres de haute qualité', 199,'2023-02-02'),
+  ('Fer à repasser', 'Fer à repasser de haute qualité avec un design ergonomique', 79,'2023-02-02'),
+  ('Grille-pain', 'Grille-pain compact avec des fonctionnalités avancées', 29,'2023-02-02'),
+  ('Machine à café', 'Machine à café professionnelle avec une vapeur puissante et une buse de distribution de lait', 199,'2023-02-02'),
+  ('Réfrigérateur', 'Réfrigérateur moderne avec un espace de stockage élargi et une température constante', 799,'2023-02-02'),
+  ('Lave-vaisselle', 'Lave-vaisselle silencieux et efficace avec un nettoyage en profondeur', 599,'2023-02-02'),
+  ('Lave-linge', 'Lave-linge de haute qualité avec une capacité de charge importante et des programmes de lavage avancés', 499,'2023-02-02'),
+  ('Chauffage dappoint', 'Chauffage dappoint efficace et économique avec un design moderne', 149,'2023-02-02'),
+  ('Ventilateur', 'Ventilateur silencieux avec une vitesse réglable et un design élégant', 59,'2023-02-02'),
+  ('Aspirateur à main', 'Aspirateur à main léger et maniable avec une aspiration puissante', 59,'2023-02-02'),
+  ('Micro-ondes', 'Micro-ondes avec un design moderne et une fonction de décongélation rapide', 49,'2023-02-02'),
+  ('Sèche-cheveux', 'Sèche-cheveux professionnel avec une vitesse et une température réglables', 59,'2023-02-02');
+
+
+
+
+
+
+INSERT INTO `utilisateurs` (`Mail`, `Mdp`, `NOM`, `PRENOM`, `Token`) VALUES
+('coubresobrugra-2555@yopmail.com', '&53O1S8ZBuvC', 'Thierry', 'Boudet', 'fd0cbb775736cc31c9953a262196f5f8be86408553727f0063728f4b0ff02425'),
+('feuddaussesaucu-8459@yopmail.com', '7@^sMD5A6ZT5', 'Arthur', 'LeBeau', 'a51ced283bc1d10ea4b151360896c4996e49822f596b97de85b5b4a748ed3915'),
+('frekossafraucrei-6311@yopmail.com', 'eR^x*995293^', 'Germain', 'Vaugrenard', '2c15f09c1ede05cf2c993c65920f9d402b4c5444324eb1380b3d0230143c155a'),
+('froitrelezeuffu-7296@yopmail.com\r\n\r\n', '7fDjvAj0%N05', 'Jacquet', 'CÃ´tÃ©', '20792474ea47c5c86904e3981184af6a3dc2f1c57a230aede438524ba95d6ed1'),
+('haddegruttaucroi-3170@yopmail.com', 'V1C8m8O@NLed', 'Marine', 'Montgomery', 'ec2c8667f529ef96c41e26ac1369f82a53549b2fc19cbdddc86831b26d86e75a'),
+('iliesskalli2@gmail.com\n', '123456', 'Skalli', 'Ilies', '488e320be4da1404b6e5d242130942fa082924fdbe38e7fb9f7745008afdf0bb'),
+('kequacripude-7099@yopmail.com', '97A^y8S&ch4*', 'Lise', 'Cousteau', '4bd61adbf06dea775c93fecfb156e3dba269cb4243a03a2200b472214beab005'),
+('lettidefreuffu-4654@yopmail.com', 'vUyy69Z73%^2', 'Ãlie', 'Jaubert', 'dd7eb5d0f3a4f949e7967222911c23f03196d9a77d7c2397268512e0ee8f452c'),
+('sideddeffape-1877@yopmail.com', '#6g54FRMmW6l', 'Milo', 'Jullien', '9cb083d7d017641d364c1603eb2deab1043823d68b8b1184068bf66e13c1d947'),
+('siyenougreha-6743@yopmail.com', '6ahy00EAb&gM', 'Alban', 'Delaunay', '178b5a92b23a495a044e55763dace527a6a63a13f957ed36f53d5235505e2446'),
+('trabeikibrauta-5448@yopmail.com', 'U358*Pb0iD$2', 'Nathan', 'Bourque', 'e869845627015cd67e56fae40f8ecf8223767577df02fc6a4aa0fd32f2417687'),
+('vossaffatteipe-3236@yopmail.com', '26Th7*3olDCC', 'Ameline', 'NaudÃ©', '566ea0a2b3d8b98471a8fafdd20db4998d0c36882d68bb67aedd36e4d1a4e7e5'),
+('wenninnauladdo-5339@yopmail.com', 'jK7k$7My3M$0', 'BÃ©atrice', 'Bazalgette', 'cd1ed676016bfb465e8a03e5bf147147c943c36f5898ca9fe3109ffd9822cd6c'),
+('wourixoyupeu-1770@yopmail.com', '4Cof3GaI3y*X', 'Hugues', 'AzÃ©ma', 'bfea844ac5b568f7435cfed6e101347708d09d2c869157844273e5cc73721ada');
+
+
+
+
+
+
+
